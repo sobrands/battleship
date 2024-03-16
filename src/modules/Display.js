5afbbb1f3b23563fa094adb430ebeec200f6378d
brandon <lim.wx.brandon@gmail.com>
2024-03-16 23:13:48 +0800

    Created the grids

--- src/modules/Display.js
+++ src/modules/Display.js
@@ -3,9 +3,6 @@ const COL_INDEX = ['', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
 export default function DisplayController() {
   function makeBoard(player) {
-    const BOARD_SIZE = 10;
-    const CELL_SIZE = 32;
-    const BOARD_AREA = CELL_SIZE * 11;
     let gameBoard = null;
     if (player === 'player') {
       gameBoard = document.getElementById('player-grid');
@@ -13,25 +10,39 @@ export default function DisplayController() {
       gameBoard = document.getElementById('opponent-grid');
     }
 
-    gameBoard.classList.add(`w-[${BOARD_AREA}px]`, `h-[${BOARD_AREA}px]`, 'grid', 'grid-cols-11', 'grid-rows-10');
+    const rowIndex = gameBoard.querySelector('#row-index');
+    const colIndex = gameBoard.querySelector('#col-index');
+    const cells = gameBoard.querySelector('#cells');
 
+    // Populate first row
     for (let i = 0; i < 11; i++) {
       const cell = document.createElement('div');
-      cell.textContent = `${COL_INDEX[i]}`;
-      cell.classList.add('cell', `w-[${CELL_SIZE}px]`, `h-[${CELL_SIZE}px]`, 'text-sm', 'self-center');
-      gameBoard.appendChild(cell);
+      cell.textContent = COL_INDEX[i];
+      cell.classList.add('cell', 'w-8', 'h-8');
+      cell.classList.add('text-xs', 'font-light');
+      cell.classList.add('flex', 'justify-center', 'items-center');
+      colIndex.appendChild(cell);
     }
 
-    for (let i = 0; i < BOARD_SIZE; i++) {
+    // Populate remaining rows
+    for (let i = 0; i < 10; i++) {
+      // Create numbered column first
       const cell = document.createElement('div');
       cell.textContent = `${ROW_INDEX[i]}`;
-      cell.classList.add('cell', `w-[${CELL_SIZE}px]`, `h-[${CELL_SIZE}px]`, 'text-sm');
-      gameBoard.appendChild(cell);
-
-      for (let j = 0; j < BOARD_SIZE; j++) {
-        const colCell = document.createElement('div');
-        colCell.classList.add('cell', `w-[${CELL_SIZE}px]`, `h-[${CELL_SIZE}px]`, 'border');
-        gameBoard.appendChild(colCell);
+      cell.classList.add('cell', 'w-8', 'h-8');
+      cell.classList.add('text-xs', 'font-light');
+      cell.classList.add('flex', 'justify-center', 'items-center');
+      rowIndex.appendChild(cell);
+      for (let j = 0; j < 10; j++) {
+        const emptyCell = document.createElement('div');
+        emptyCell.classList.add('cell', 'w-8', 'h-8');
+        if (i < 9) {
+          emptyCell.classList.add('border-b');
+        }
+        if (j < 9) {
+          emptyCell.classList.add('border-r');
+        }
+        cells.appendChild(emptyCell);
       }
     }
   }
